e801ebd59ac3adf31f11c9824e00cb4e38f51b6b
MuriloBC2 <murilobarrosdocarmo@gmail.com>
2023-07-02 21:39:41 -0300

    adição dos icons

--- Spotify_clone/App.js
+++ Spotify_clone/App.js
@@ -1,34 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import MaterialIcon from "@expo/vector-icons/MaterialIcon"
-import HomeScreen from "./screens/HomeScreen";
-import SearchScreen from "./screens/SearchScreen";
-import LibaryScreen from "./screens/LibraryScreen";
-import PremiumScreen from "./screens/PremiumScreen";
+import Navigation from './StackNavigator';
+
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator initialRouteName="Home"
-      screenOptions={{
-          tabBarstyle:{
-            height: 65,
-            paddingTop:10,
-            backgroundColor:"rgb(0,0,0)",
-            borderTopwidth:0,
-          },
-          
-      }}>
-        <Tab.Screen name ="Home" component={HomeScreen}/>
-        <Tab.Screen name="Search" component={SearchScreen}/>
-        <Tab.Screen name="Library" component={LibaryScreen}/>
-        <Tab.Screen name="Premium" component={PremiumScreen}/>
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
+    <>
+      <Navigation/>
+    </>
+  )          
 }
+
 
 const styles = StyleSheet.create({
   container: {
--- Spotify_clone/StackNavigator.js
+++ Spotify_clone/StackNavigator.js
@@ -1,27 +1,61 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "./screens/HomeScreen"
+import ProfileScreen from "./screens/ProfileScreen"
 import { Entypo } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons'; 
+import { Ionicons } from "@expo/vector-icons";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
 
 
 const Tab = createBottomTabNavigator();
 
 function Bottomtabs(){
-    return(
-        <Tab.Navigator>
-            <Tab.Screen  name="Home" 
-            component={HomeScreen}
-             options={{tabBarLabel:"Home", headerShown:false, 
-             tabBarLabelStyle:{color:"white"},
-             tabBarIcon:({focused}) =>
-             focused ? {
+    return (
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarLabel: "Home",
+            headerShown: false,
+            tabBarLabelStyle: { color: "white" },
+            tabBarIcon: ({ focused }) =>
+              focused ? (
                 <Entypo name="home" size={24} color="black" />
-            } : {
+              ) : (
                 <AntDesign name="home" size={24} color="black" />
-                 }
-            }}
-             />
-            <Tab.Screen name="Profile" component={}/>
-        </Tab.Navigator>
+              ),
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{
+            tabBarLabel: "Home",
+            headerShown: false,
+            tabBarLabelStyle: { color: "white" },
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <Ionicons name="person" size={24} color="black" />
+              ) : (
+                <Ionicons name="person-outline" size={24} color="black" /> 
+              ),
+          }}
+        />
+      </Tab.Navigator>
+    );
+}
+
+const Stack = createNativeStackNavigator();
+function Navigation (){
+    return (
+        <NavigationContainer>
+            <StackNavigation>
+                <Stack.Screen name="Main" component={Bottomtabs} options={{headerShown:false}}/>
+            </StackNavigation>
+        </NavigationContainer>
     )
 }
+
+export default Navigation(); 

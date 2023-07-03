2b0334953e7c4347172ca936e11daea8a563452d
MuriloBC2 <murilobarrosdocarmo@gmail.com>
2023-07-03 00:17:30 -0300

    continuação da modificação

--- Spotify_clone/App.js
+++ Spotify_clone/App.js
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Navigation from './StackNavigator';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { ModalPortal } from "react-native-modals";
+import { PlayerContext } from "./PlayerContext";
+import Navigation from "./StackNavigator";
 
 export default function App() {
   return (
     <>
-      <Navigation/>
+      <PlayerContext>
+        <Navigation />
+        <ModalPortal />
+      </PlayerContext>
     </>
-  )          
+  );
 }
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ Spotify_clone/screens/LoginScreen.js
@@ -0,0 +1,13 @@
+import {StyleSheet, Text, View, SafeAreaView} from 'react-native'
+import react from 'react'
+
+const LoginScreen = () => {
+    return(
+        
+    )
+
+}
+
+export default LoginScreen
+
+const styles = StyleSheet.create({})
